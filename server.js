7f067a1771bc3f6e81299c6dd7e73803afea812c
SofianeDjellouli <38258952+SofianeDjellouli@users.noreply.github.com>
2018-09-01 17:09:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
-// Security with helmet
-helmet.noSniff();
-helmet.xssFilter()
-
 var app = express();
+
+// Security with helmet
+app.use(helmet.noSniff())
+app.use(helmet.xssFilter())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
